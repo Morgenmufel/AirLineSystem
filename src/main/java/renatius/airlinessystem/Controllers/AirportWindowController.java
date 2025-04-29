eba9a5391cb013a3d4f5ad4ab0e37abc7032bb8a
Yan <yan.com142@gmail.com>
2025-04-29 09:33:17 +0300

    enum dont work

--- src/main/java/renatius/airlinessystem/Controllers/AirportWindowController.java
+++ src/main/java/renatius/airlinessystem/Controllers/AirportWindowController.java
@@ -1,14 +1,22 @@
 package renatius.airlinessystem.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import renatius.airlinessystem.Entity.Enum.WeatherStatus;
 import renatius.airlinessystem.Entity.GroundUnit.Airport;
+import renatius.airlinessystem.services.impl.AirportServiceImpl;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class AirportWindowController {
@@ -64,7 +72,45 @@ public class AirportWindowController {
     @FXML
     private Button exitButton;
 
-    public void AddAirport(){};
+    @FXML
+    public void initialize(){
+        AirportServiceImpl airportService = new AirportServiceImpl();
+        ObservableList<Airport> airportsList = null;
+        try{
+            airportsList = FXCollections.observableArrayList(airportService.getAllAirport());
+        }catch (Exception e){
+            error_add_label.setText("Список пуст");
+            e.printStackTrace();
+        }
+        AirportTableView.setItems(airportsList);
+        name_column.setCellValueFactory(new PropertyValueFactory<>("name"));
+        country_column.setCellValueFactory(new PropertyValueFactory<>("country"));
+        status_column.setCellValueFactory(new PropertyValueFactory<>("weatherStatus"));
+        ViewAirports();
+        WeatherStatus weatherStatus [] = WeatherStatus.values();
+        List<String> statusList = Arrays.stream(weatherStatus).map(Enum::name).collect(Collectors.toList());
+        choose_status_box.getItems().addAll(statusList);
+        rechoose_status_box.getItems().addAll(statusList);
+    }
+
+    public void ViewAirports(){
+        AirportServiceImpl airportService = new AirportServiceImpl();
+        ObservableList<Airport> airportsList = FXCollections.observableArrayList(airportService.getAllAirport());
+        AirportTableView.getItems().clear();
+        AirportTableView.setItems(airportsList);
+    }
+
+    public void AddAirport(){
+        AirportServiceImpl airportService = new AirportServiceImpl();
+        Airport airport = new Airport();
+        airport.setName(name_field.getText());
+        airport.setCountry(country_field.getText());
+        WeatherStatus weatherStatus = WeatherStatus.valueOf(choose_status_box.getValue());
+        System.out.println(weatherStatus);
+        airport.setWeatherStatus(weatherStatus);
+        airportService.addAirport(airport);
+        ViewAirports();
+    }
 
     public void EditAirport(){};
 
